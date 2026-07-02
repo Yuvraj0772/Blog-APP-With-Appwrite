58f4ceacdba5c0bf3e99322b2c686ec8874e1f8a
Yuvraj Singh Chouhan <yuvrajsinghchouhan9669@gmail.com>
2026-07-02 20:58:09 +0530

    configer reduxtoolkit

---
+++ src/components/Footer/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function Footer() {
+  return (
+    <footer>
+      <p>&copy; 2024 React MegaProject. All rights reserved.</p>
+    </footer>
+  );
+}
+
+export default Footer;
